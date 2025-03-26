47f0795b28dab602e22395a1464c1debadd0174a
Cal <lordcalwong@gmail.com>
2025-03-25 20:57:50 -0700

    show measurement required firs
    avoid exception error by making item active

--- mysample.py
+++ mysample.py
@@ -32,55 +32,58 @@ print (f"Max Input Channels: {maxInputChCount}")
 APx.SignalPathSetup.AnalogInputChannelCount = maxInputChCount
 APx.SignalPathSetup.AnalogOutputChannelCount = maxOutputChCount
 
-# Set 
-APx.AddMeasurement("Signal Path1", MeasurementType.FrequencyResponse)
-APx.FrequencyResponse.Level.Checked = True
+# # Set 
+# APx.AddMeasurement("Signal Path1", MeasurementType.FrequencyResponse)
+# APx.FrequencyResponse.Level.Checked = True
+
+APx.ShowMeasurement(0,"Noise (RMS)")
 APx.Noise.Level.Checked = True
+
 # easurement("Signal Path1", MeasurementType.FrequencyResponse)
 # APx.FrequencyResponse.Level.Checked = True
 
 # Add and configure an Acoustic Response measurement
-APx.AddMeasurement("Signal Path1", MeasurementType.AcousticResponse)
-APx.AcousticResponse.GeneratorWithPilot.Frequencies.Start.Value =20;
-APx.AcousticResponse.GeneratorWithPilot.Frequencies.Stop.Value = 20000;
-APx.AcousticResponse.GeneratorWithPilot.Levels.Sweep.SetValue(OutputChannelIndex.Ch1, "0.000 dBrG");
-APx.AcousticResponse.GeneratorWithPilot.Durations.Sweep.Value = 1;
+# APx.AddMeasurement("Signal Path1", MeasurementType.AcousticResponse)
+# APx.AcousticResponse.GeneratorWithPilot.Frequencies.Start.Value =20;
+# APx.AcousticResponse.GeneratorWithPilot.Frequencies.Stop.Value = 20000;
+# APx.AcousticResponse.GeneratorWithPilot.Levels.Sweep.SetValue(OutputChannelIndex.Ch1, "0.000 dBrG");
+# APx.AcousticResponse.GeneratorWithPilot.Durations.Sweep.Value = 1;
         
-#Check a couple results to be included in the active sequence
-APx.AcousticResponse.Level.Checked = True
-APx.AcousticResponse.Phase.Checked = True
-APx.AcousticResponse.ThdRatio.Checked = True
+# #Check a couple results to be included in the active sequence
+# APx.AcousticResponse.Level.Checked = True
+# APx.AcousticResponse.Phase.Checked = True
+# APx.AcousticResponse.ThdRatio.Checked = True
 
-#Add a derived result and configure it
-smooth = APx.AcousticResponse.Level.AddDerivedResult(MeasurementResultType.Smooth).Result.AsSmoothResult();
-smooth.OctaveSmoothing = OctaveSmoothingType.Octave3
-smooth.Name = "Smoothed Response"
-smooth.Checked = True
+# #Add a derived result and configure it
+# smooth = APx.AcousticResponse.Level.AddDerivedResult(MeasurementResultType.Smooth).Result.AsSmoothResult();
+# smooth.OctaveSmoothing = OctaveSmoothingType.Octave3
+# smooth.Name = "Smoothed Response"
+# smooth.Checked = True
 
 
-#Add an export data sequence step to automatically export the smoothed data when the measurement is run in a sequence.
-exportStep = APx.AcousticResponse.SequenceMeasurement.SequenceSteps.ExportResultDataSteps.Add()
-exportStep.ResultName = smooth.Name
-exportStep.ExportSpecification = "All Points"
-exportStep.FileName = "$(MyDocuments)\\SmoothedResponse.xlsx"
-exportStep.Append = False
+# #Add an export data sequence step to automatically export the smoothed data when the measurement is run in a sequence.
+# exportStep = APx.AcousticResponse.SequenceMeasurement.SequenceSteps.ExportResultDataSteps.Add()
+# exportStep.ResultName = smooth.Name
+# exportStep.ExportSpecification = "All Points"
+# exportStep.FileName = "$(MyDocuments)\\SmoothedResponse.xlsx"
+# exportStep.Append = False
 
-# Run the sequence, which will run the Acoustic Response measurement
-APx = APx500_Application()
-APx.Sequence.Run()
+# # Run the sequence, which will run the Acoustic Response measurement
+# APx = APx500_Application()
+# APx.Sequence.Run()
       
-# Get results acquired in the last sequence run
+# # Get results acquired in the last sequence run
 
-# Get Frequency Response RMS Level XY values by result name (string)
-level_xvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults["Smoothed Response"].GetXValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
-level_yvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults["Smoothed Response"].GetYValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
+# # Get Frequency Response RMS Level XY values by result name (string)
+# level_xvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults["Smoothed Response"].GetXValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
+# level_yvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults["Smoothed Response"].GetYValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
 
-# Get Frequency Response Gain XY values by result type
-thd_xvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults[MeasurementResultType.ThdRatioVsFrequency].GetXValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
-thd_yvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults[MeasurementResultType.ThdRatioVsFrequency].GetYValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
+# # Get Frequency Response Gain XY values by result type
+# thd_xvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults[MeasurementResultType.ThdRatioVsFrequency].GetXValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
+# thd_yvalues = APx.Sequence[0]["Acoustic Response"].SequenceResults[MeasurementResultType.ThdRatioVsFrequency].GetYValues(InputChannelIndex.Ch1, VerticalAxis.Left, SourceDataType.Measured, 1)
 
-# Print all level y values to show that APIs ran succesfully
-print (*level_yvalues)
+# # Print all level y values to show that APIs ran succesfully
+# print (*level_yvalues)
 
 
 # APx.SignalPathSetup.InputConnector.Type = InputConnectorType.ASIO
