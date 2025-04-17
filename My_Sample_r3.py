b9b4d368781cb98d723df5d3e4b76b462b6d64d3
Cal <lordcalwong@gmail.com>
2025-04-16 19:37:38 -0700

    Polished
    Good to Go

--- My_Sample_r3.py
+++ My_Sample_r3.py
@@ -108,65 +108,37 @@ headers = ["level_xvaluesCh2", "level_yvaluesCh2"]
 print_arrays_to_csv(level_xvaluesCh2, level_yvaluesCh2, "LevelRMS.csv", headers)
 
 
-
+# APx.ShowMeasurement(“Signal Path1”, “Acoustic Response”);
 # APx.SignalPathSetup.InputConnector.Type = InputConnectorType.ASIO
 # # APx.SignalPathSetup.InputDevice = "APx ASIO Loopback"
 # APx.SignalPathSetup.OutputConnector.Type = OutputConnectorType.ASIO
 # APx.SignalPathSetup.OutputDevice = "APx ASIO Loopback"
-
 # APx.Sequence.ApplyCheckedState(True)
 # APx.Sequence["Signal Path1"].SequenceResults["Frequency Response"].Checked = True
-
-# # APx.SignalPathSetup.LowpassFilter.A
+# APx.SignalPathSetup.LowpassFilter.A
 # APx.SignalPathSetup.WeightingFilter.A-weighting
-
-# APx.SignalPathSetup
-# APx.SignalPathSetup.OutputConnector
 # APx.AcousticResponse.Level.ExportSpecification = “1000 points“
+# APx. AcousticResponse.Level.ExportSpecification = “All Points“
 # APx.FrequencyResponse.Level.YAxis.Unit = "dBV";
 # ch1xValues = APx.Sequence["Signal Path1"][“MR"].SequenceResults["RMS Level"].GetXValues(0)
 # ch1yValues = APx.Sequence["Signal Path1"][“MR"].SequenceResults["RMS Level"].GetYValues(0)
 # meterValues = APx.Sequence["Signal Path1"][“THD+N"].SequenceResults[“THD+N Ratio"].GetMeterValues();
-
-# APx.Sequence.Run()
-# APx.AddMeasurement("Signal Path1", MeasurementType.AcousticResponse)
-# APx.AcousticResponse.GeneratorWithPilot.Frequencies.Start.Value = 100;
-# APx.AcousticResponse.GeneratorWithPilot.Frequencies.Stop.Value = 10000;
-# APx.AcousticResponse.GeneratorWithPilot.Levels.Sweep.SetValue(OutputChannelIndex.Ch1, "1 Vrms");
-# APx.AcousticResponse.GeneratorWithPilot.Durations.Sweep.Value = 1;
-
-# APx = APx500_Application()
 # APx.SignalPathSetup.OutputConnector.Type = OutputConnectorType.AnalogBalanced
 # APx.SignalPathSetup.InputConnector.Type = InputConnectorType.AnalogBalanced 
 # APx.SignalPathSetup.Measure = MeasurandType.Acoustic
 # input1 = APx.SignalPathSetup.InputSettings(APxInputSelection.Input1)
 # input1.Channels[0].Name = "Mic"
 # input1.Channels[0].Sensitivity.Value = 0.011
-
-# (APxOperatingMode.SequenceMode)
 # APx.MeasurementRecorder.LevelVsTime
 # APx.MeasurementRecorder.Graphs["RMS Level (PDM 16)"].Result.AsXYGraph().GetAllXValues
-# .GetAllYValues
-
-# APx. AcousticResponse.Level.ExportSpecification = “All Points“
-# APx. AcousticResponse.Level.ExportSpecification = “1000 points“
 # APx.FrequencyResponse.Level.YAxis.Unit = "dBV";
-
-# APx.ShowMeasurement(“Signal Path1”, “Acoustic Response”);
 # var xVals = APx.AcousticResponse.Level.GetAllXValues(InputChannelIndex.Ch1);
 # var yVals = APx.AcousticResponse.Level.GetAllYValues(InputChannelIndex.Ch1);
-
 # APx.AcousticResponse.Level.ExportData("c:\\data\\level.xlsx", “All Points");
-# APx.Sequence.Run();
-
 # APx.Sequence["Signal Path1"][“MR"].SequenceResults[“RMS Level”].Checked = true;
-# APx.Sequence.Run()
-
 # ch1xValues = APx.Sequence["Signal Path1"][“MR"].SequenceResults["RMS Level"].GetXValues(0)
 # ch1yValues = APx.Sequence["Signal Path1"][“MR"].SequenceResults["RMS Level"].GetYValues(0)
-
 # meterValues = APx.Sequence["Signal Path1"][“THD+N"].SequenceResults[“THD+N Ratio"].GetMeterValues();
-
 # SettlingMeterType[] meterTypes = { SettlingMeterType.RmsLevel, SettlingMeterType.ThdNRatio };
 # var settledResults = APx.BenchMode.GetSettledMeterReadings(meterTypes);
 # var lvlValues = settledResults[SettlingMeterType.RmsLevel].GetValues("Vrms");
@@ -174,7 +146,6 @@ print_arrays_to_csv(level_xvaluesCh2, level_yvaluesCh2, "LevelRMS.csv", headers)
 
 # APx.BenchMode.Measurements.Recorder.Show()
 # APx.BenchMode.Measurements.Recorder.Start()
-
 # APx.BenchMode.Measurements.Recorder.Start();
 # while (APx.BenchMode.Measurements.Recorder.IsStarted)
 #   Thread.Sleep(100);
